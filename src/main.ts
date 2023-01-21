593964176171e83294419381664cebe85aace58a
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2023-01-21 20:25:33 +0530

    OITS fixing origin url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost"5173', credentials: true });
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
   app.use(cookieParser());
   await app.listen(3000);
 }
