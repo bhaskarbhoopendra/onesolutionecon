3243aabb22cb38c2a34edc4411782a11cc7ed143
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2023-01-22 13:05:00 +0530

    category crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [DatabaseModule, AdminModule, ConfigModule.forRoot()],
+  imports: [
+    DatabaseModule,
+    AdminModule,
+    ConfigModule.forRoot(),
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  modified: Date;
+}
